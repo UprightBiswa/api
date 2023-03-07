ed0148bdae90ed265f06ce680467a4af33d2c275
Biswajit Das <90900262+UprightBiswa@users.noreply.github.com>
2023-03-08 02:46:23 +0530

    token

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\StudentController;
 
 /*
@@ -32,6 +33,23 @@ use App\Http\Controllers\StudentController;
 // Route::delete('/students/{id}',[StudentController::class, 'destroy']);
 // Route::get('/students/search/{city}',[StudentController::class, 'search']);
 
+Route::post('/register',[UserController::class, 'register']);
+
 // protected routes
 Route::middleware('auth:sanctum')->get('/students',[StudentController::class, 'index']);
 
+
+// protected routes
+// Route::middleware('auth:sanctum')->get('/students',[StudentController::class, 'index']);
+// Route::middleware('auth:sanctum')->get('/students/{id}',[StudentController::class, 'show']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/students',[StudentController::class, 'index']);
+    Route::get('/students/{id}',[StudentController::class, 'show']);
+    Route::post('/students',[StudentController::class, 'store']);
+    Route::put('/students/{id}',[StudentController::class, 'update']);
+    Route::delete('/students/{id}',[StudentController::class, 'destroy']);
+    Route::get('/students/search/{city}',[StudentController::class, 'search']);
+
+
+});
